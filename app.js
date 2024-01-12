282665b9b2608078e9903cb585f8ad9113fc63a3
vector-10 <duzieblaise10@gmail.com>
2024-01-12 01:24:22 +0100

    chore: Tailwind setup for React.js

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const sampleDataRoute = require('./routes/sampleRoute');
+//const sampleDataRoute = require('./routes/sampleRoute');
 
 // Middleware
 app.use(bodyParser.json());
 
 // Routes
 app.get('/', (req, res) => {
-  res.send('Hello, world!');
+  res.json({
+    message: "Hello from Server"
+  })
 });
 
-app.get('/api/v1', sampleDataRoute);
+//app.get('/api/v1', sampleDataRoute);
 
 
-// Start the server
+// start the server process
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
