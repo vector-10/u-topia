ab270b7e878bd63d84d0f166c48ae2c4c1037bb7
vector-10 <duzieblaise10@gmail.com>
2024-01-23 04:34:57 +0100

    chore: model update

--- models/companyModel.js
+++ models/companyModel.js
@@ -1,6 +1,39 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const companySchema = new mongoose.Schema({
+    companyName:{
+        type: String,
+        required: [true, "Please provide a ccompany name"],
+        minlength: [5, "COmpany name should be more than 5 characters"],
+        maxlength: [30, "Company name cannot exceeed 30 characters"]
+    },
+    contact: {
+        type: Number,
+        required:  [true,"please provide a contact phone nu`mber"]
+    },
+    contactEmail:{
+        type: String,
+        required:  [true,"Please provide a valid email address"],
+        unique: [true, "Please provide a unique email address"],
+        validate: [validator.isEmail, "Please provide a valid email"]
+    },
+    industry: {
+        type: String,
+        required: true,
+      },
+      companyDescription: {
+        type: String,
+        required: true,
+      },
+      websiteURL: {
+        type: String,
+      },
+      companyAddress: {
+        type: String,
+      },
+      
+
 
 })
 
